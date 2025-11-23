b91250a574e17183ac62ebb7d3e08b2bd5d8672b
AliasJeff <77962635+AliasJeff@users.noreply.github.com>
2025-11-23 11:07:04 +0800

    fix: pr diff api (#18)

--- ai-rag-knowledge/rag-dev-tech-app/src/main/java/com/alias/rag/dev/tech/RagApplication.java
+++ ai-rag-knowledge/rag-dev-tech-app/src/main/java/com/alias/rag/dev/tech/RagApplication.java
@@ -3,9 +3,11 @@ package com.alias.rag.dev.tech;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @Configurable
+@ComponentScan(basePackages = {"com.alias.rag.dev.tech", "com.alias.rag.dev.tech.trigger"})
 public class RagApplication {
 
   public static void main(String[] args) {
